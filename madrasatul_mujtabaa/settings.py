f5020415806a0fb439e67dde2c4d2ecd0c6f8ddb
mrisho2348 <mrishohamisi2348@gmail.com>
2023-10-28 04:00:08 +0300

    aws

--- madrasatul_mujtabaa/settings.py
+++ madrasatul_mujtabaa/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.humanize',
     'django.contrib.staticfiles',
     'student_management_app',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
